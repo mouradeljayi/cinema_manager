71ed2deb423c5c2523d2704463653e31163bf0cc
mouradeljayi <mouradeljayi98@gmail.com>
2023-08-27 12:57:28 +0100

    setup environment

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+require('dotenv').config()
+const cors = require('cors')
+
+const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const port = process.env.PORT || 5000
+
+connectDB()
+
+const app = express()
+app.use(cors())
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.use(errorHandler)
+
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
